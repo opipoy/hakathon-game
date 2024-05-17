69b5f10624cc81044ed3c41b86efc797586f235d
Your Name <you@example.com>
2024-05-17 19:41:59 +0300

    - made the phisics more realistic - uses sprites now to check colisions, which makes the code more simple

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pygame
 from objects import *
 
 levels = {0 : [
-    {'obj': ground, 'pos': (0, 700), 'size': (1000, 100)},
-    {'obj': ground, 'pos': (600, 600), 'size': (100, 100)},
-    {'obj': player, 'pos':(0, 700), 'size': (100, 100)},
-    {'obj': player, 'pos': (10, 10), 'size': (5, 5)}
+    {'obj': ground, 'pos': (0, 700), 'size': (500, 50)},
+   # #{'obj': inviz_ground, 'pos': (500, 700), 'size': (100, 100)},
+    {'obj': player, 'pos':(0, 250), 'size': (100, 100)},
+   {'obj': player, 'pos': (200, 10), 'size': (10, 10)}
                ]
 }
 
@@ -14,29 +14,27 @@ levels = {0 : [
 pygame.init()
 
 def update_obj_on_lvl():
-    for objec in obj_list:
-        objec.update()
+    global dt
+    obj_list.update()
+    obj_list.draw(screen)
 
 
 screen = pygame.display.set_mode((1000, 800))
 
 # p = player(screen=screen)
 # p.scale(100, 100)
-# p.make_on_pos(10, 10)        
+# p.make_on_pos(10, 10)
 
 # g = ground(screen=screen)
 
 # g.scale(1000, 100)
 # g.make_on_pos(0, 700)
+#יוצר אובייקים בשלב
 for lvl in levels[0]:
-
-    objec = lvl["obj"](screen)
-    if player.__name__ == lvl["obj"].__name__:
-        players.append(objec)
-    objec.make_on_pos(*lvl["pos"])
+    objec = lvl["obj"](screen, **lvl["options"] if "options" in lvl else {})
+    objec.rect.x, objec.rect.y = lvl["pos"]
     objec.scale(*lvl["size"])
-    obj_list.append(objec)
-    objec.update()
+    obj_list.add(objec)
 
 running = True
 
@@ -48,19 +46,19 @@ while running:
     key = pygame.key.get_pressed()
     if key[pygame.K_RIGHT]:
         for p in players:
-            p.velocity[0] = 1
+            p.velocity[0] = 20/0.6 
     if key[pygame.K_LEFT]:
         for p in players:
-            p.velocity[0] = -1
+            p.velocity[0] = -20/0.6
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             for p in players:
                 key = pygame.key.get_pressed()
                 if p.on_ground:
-                    if key[pygame.K_UP]:
-                            p.move(y=-0.5)
+                    print(p.on_ground)
+                    if key[pygame.K_SPACE]:
+                            p.velocity[1] = -20 - p.weight/50
         if event.type == pygame.QUIT:
             running = False
+    dt = clock.tick(30)
     pygame.display.flip()
-
-
--- objects/objects.py
+++ objects/objects.py
@@ -1,92 +1,102 @@
-
 import pygame
-solids = []
-obj_list = []
-players = []
+solids = pygame.sprite.Group()
+players = pygame.sprite.Group()
+obj_list = pygame.sprite.Group()
+clock = pygame.time.Clock()
+dt = 0
 
 
-class template_obj():
+class template_obj(pygame.sprite.Sprite):
     def make_colisions(self):
         for colision_name in self.template_colisions.keys():
             template = self.template_colisions[colision_name]
-            colision = pygame.Rect(self.x + template[0], self.y + template[1], self.W + template[2], self.H + template[3])
+            colision = pygame.Rect(self.rect.x + template[0], self.rect.y + template[1], self.rect.w + template[2], self.rect.h + template[3])
             pygame.draw.rect(self.screen, "blue", colision)
             self.colisions[colision_name] = colision
+
     def __init__(self, img, screen):
+        super().__init__()
         self.IMG = img
         self.screen = screen
-        try:
-            self.loaded_img = pygame.image.load(img)
-        except pygame.error:
-            print(f"Error: Image file '{img}' not found.")
-            # Handle the error gracefully, e.g. by using a default image or displaying an error message to the user.
-        self.H = self.loaded_img.get_height()
-        self.W = self.loaded_img.get_width()
-        self.x, self.y = 0 ,0
+        self.image = pygame.image.load(img)
+        self.rect = pygame.Rect(0, 0, 0, 0)
+        self.rect.h = self.image.get_height()
+        self.rect.w = self.image.get_width()
+        self.rect.x, self.rect.y = 0, 0
         self.colisions = {}
         #                                     x  y  w  h
         self.template_colisions = {'sprite': (0, 0 ,0 ,0)}
         self.make_colisions()
 
-    def make_on_pos(self, x, y):
-        self.screen.blit(self.loaded_img, (x, y))
-        self.x, self.y = x, y
-        self.make_colisions()
     def scale(self, width, height):
-        self.W, self.H = width, height
-        self.loaded_img = pygame.transform.scale(self.loaded_img, (width, height))
+        self.rect.w, self.rect.h = width, height
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.make_colisions()
-
-    def check_colision(self, rect_list):
-        colisions_touching = []
-        for colision_name in self.colisions.keys():
-                colision = self.colisions[colision_name]
-                if len(colision.collidelistall(rect_list)) > 0:
-                    colisions_touching.append(colision_name)
-        return colisions_touching
-
 
 class player(template_obj):
-    def __init__(self, screen):
+    def __init__(self, screen, speed = 0, density = 0, gravity = 0,):
         self.jumping = False
         self.screen = screen
         self.velocity = [0, 0]
-        self.gravity = 0.003
+        self.gravity = gravity or 2
         self.on_ground = False
-
-        super().__init__(r"./recorces/player.gif" ,screen)
+        self.touching_grounds = []
+        self.pos_on_list = len(solids)
+        super().__init__(r"./recorces/player.gif", screen)
+        self.density = density or 0.1
+        self.weight = self.rect.w * self.rect.h * self.density
+        players.add(self)
        
-    def move(self, x = None, y = None):
-        self.velocity[0] += x if x != None else self.velocity[0]
-        self.velocity[1] += y if y != None else self.velocity[1]
+    def move(self, x = 0, y = 0):
+        self.velocity[0] += x
+        self.velocity[1] += y
     
     def fall(self):
-        if self.on_ground & ( self.velocity[1] > 0 ):
+        if self.on_ground & ( self.velocity[1] >= 0 ):
+            ground_y =  self.touching_grounds[0].rect.y
+            y =  self.rect.y
+            h = self.rect.h
+            self.rect.y = ground_y - h+0.5
             self.velocity[1] = 0
+        elif self.velocity[1] <= self.weight:
+            self.move(y= 4*dt^2)
         else:
-            self.move(y= self.gravity)
+            self.velocity[1] = self.weight
     
     def friction(self):
-        if  abs(self.velocity[0]) > 0.01:
-            self.velocity[0] *= 0.3
+        if  abs(self.velocity[0]) > 0.1:
+            self.velocity[0] *= 0.6
         else:
             self.velocity[0] = 0
         
     def update(self):
-        self.on_ground = len( self.check_colision(solids) ) > 0
-        self.friction()
+        #solids.append(self.colisions["sprite"])
+        self.touching_grounds = pygame.sprite.spritecollide(self, solids, False)
+        self.on_ground = len( self.touching_grounds ) > 0
         self.fall()
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
-        self.make_on_pos(self.x, self.y)
-    
-    
+        self.friction()
+        self.weight = self.rect.w * self.rect.h * self.density
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        pygame.draw.rect(self.screen, "green", self.rect)
+        pygame.draw.rect(self.screen, "yellow", self.colisions["sprite"])
+
 class ground(template_obj):
     def __init__(self, screen):
-        super().__init__(r"./recorces/player.gif" ,screen)
-        self.pos_on_list = len(solids)
-        solids.append(self.colisions["sprite"])
-        
+        super().__init__(r"./recorces/player.gif", screen)
+        solids.add(self)
+
     def update(self):
-        solids[self.pos_on_list] = self.colisions["sprite"]
-        self.make_on_pos(self.x, self.y)
+        pygame.draw.rect(self.screen, "yellow", self.colisions["sprite"])
+
+def game_over(screen):
+    imp = pygame.image.load(r"D:\Users\user\Downloads\hakathon-game-Ofeks\recorces\game over.jpg").convert()
+    pygame.display.set_mode((imp.get_width(),imp.get_height()))
+    screen.blit(imp, (0,0))
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
